d592da9951288190a6e3dda53ac77a8e5ba8acd6
SeanFeng91 <fengyx91@gmail.com>
2024-07-05 17:33:02 +0800

    加入TailwindCss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SeanFeng91.github.io',
   base: '/SeanBlog/',
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
