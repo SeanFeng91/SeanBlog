4955d96687827c7dcc8c0e5e8ef900119a3fe78b
SeanFeng91 <fengyx91@gmail.com>
2024-07-08 14:30:01 +0800

    研究了不同模板和sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  // content: [],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
+// export default {
+//   // content: [],
+//   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','node_modules/preline/dist/*.js'],
+
+  theme: {
+    extend: {
+      // Set font family
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      // Set theme colors (Required config!)
+      colors: {
+        primary: colors.blue,
+        secondary: colors.slate,
+      },
+    },
+  },
+  // Add plugins
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"),require('preline/plugin')],
+};
+      
