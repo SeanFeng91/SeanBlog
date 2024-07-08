b3806fd3e4f33498718f73364fc6737daed46fb0
SeanFeng91 <fengyx91@gmail.com>
2024-07-08 15:01:28 +0800

    研究了不同模板和sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,41 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
-// export default {
-//   // content: [],
-//   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-module.exports = {
-  // content: ["./src/**/*.{html,js}"],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','node_modules/preline/dist/*.js'],
-
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/preline/preline.js',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
-      // Set font family
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      // Set theme colors (Required config!)
-      colors: {
-        primary: colors.blue,
-        secondary: colors.slate,
+        sans: [
+          'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji',
+        ],
       },
     },
   },
-  // Add plugins
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"),require('preline/plugin')],
+  plugins: [require("@tailwindcss/typography"),require('@tailwindcss/forms'), require('preline/plugin')],
 };
+// module.exports = {
+//   // content: ["./src/**/*.{html,js}"],
+//   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','node_modules/preline/dist/*.js'],
+
+//   theme: {
+//     extend: {
+//       // Set font family
+//       fontFamily: {
+//         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+//       },
+//       // Set theme colors (Required config!)
+//       colors: {
+//         primary: colors.blue,
+//         secondary: colors.slate,
+//       },
+//     },
+//   },
+//   // Add plugins
+//   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"),require('preline/plugin')],
+// };
       
