a6237c8beb1ce5bc28ecbc5ee2ed8da80c55b6a9
SeanFeng91 <fengyx91@gmail.com>
2024-08-09 14:42:47 +0800

    增加感悟和摘录

---
+++ src/scripts/background.js
@@ -0,0 +1,7 @@
+import { AestheticFluidBg } from "./AestheticFluidBg.module.js";
+
+let colorbg = new Color4Bg.AestheticFluidBg({
+	dom: "box",
+	colors: ["#FF9113","#F6E384","#F41AA7","#0728E9","#27E224","#FF9113"],
+	loop: true
+})
