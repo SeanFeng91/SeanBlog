d0c6fef604041f6154f1675c3a153a156defc56b
SeanFeng91 <fengyx91@gmail.com>
2024-09-14 11:09:02 +0800

    新西兰路书

--- src/components/react/Video.jsx
+++ src/components/react/Video.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const Video = ({ src, type = 'video/mp4', youtube, bilibili, width = '100%', height = 'auto' }) => {
+const Video = ({ 
+  src, 
+  type = 'video/mp4', 
+  youtube, 
+  bilibili, 
+  width = '100%', 
+  height = 'auto', 
+  autoplay = false 
+}) => {
   if (youtube) {
+    const youtubeUrl = `https://www.youtube.com/embed/${youtube}${autoplay ? '?autoplay=1' : ''}`;
     return (
       <iframe
         width={width}
         height={height}
-        src={`https://www.youtube.com/embed/${youtube}`}
+        src={youtubeUrl}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
@@ -15,22 +24,22 @@ const Video = ({ src, type = 'video/mp4', youtube, bilibili, width = '100%', hei
   }
 
   if (bilibili) {
+    const bilibiliUrl = `https://player.bilibili.com/player.html?bvid=${bilibili}&high_quality=1&danmaku=0&autoplay=${autoplay ? 1 : 0}`;
     return (
       <iframe
-        src={`https://player.bilibili.com/player.html?bvid=${bilibili}&high_quality=1&danmaku=0`}
+        src={bilibiliUrl}
         width={width}
         height={height}
-        scrolling="no"
-        border="0"
-        frameBorder="no"
-        framespacing="0"
+        frameBorder="0"
+        allow="fullscreen"
         allowFullScreen
+        style={{ border: 'none', overflow: 'hidden' }}
       ></iframe>
     );
   }
 
   return (
-    <video width={width} height={height} controls>
+    <video width={width} height={height} controls autoPlay={autoplay}>
       <source src={src} type={type} />
       Your browser does not support the video tag.
     </video>
