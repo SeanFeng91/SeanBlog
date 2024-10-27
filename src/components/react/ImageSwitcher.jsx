92f52aaecde9cec5b209b51ff89c74b629a7f34c
SeanFeng91 <fengyx91@gmail.com>
2024-10-27 19:46:21 +0800

    修复翻页bug

--- src/components/react/ImageSwitcher.jsx
+++ src/components/react/ImageSwitcher.jsx
@@ -1,40 +1,30 @@
 import React, { useState } from 'react';
 
-// ImageSwitcher组件：用于在多张图片之间切换显示
-// props:
-//   images: 图片URL数组
-//   maxHeight: 图片容器的最大高度，默认为3000px
-const ImageSwitcher = ({ images, maxHeight = 3000 }) => {
-  // 使用useState钩子来管理当前显示的图片索引
+const ImageSwitcher = ({ images, maxHeight = 1000 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // 切换到下一张图片
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  // 切换到上一张图片
   const prevImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   return (
     <div className="flex flex-col items-center w-full">
-      {/* 图片容器 */}
       <div 
         style={{ maxHeight: `${maxHeight}px` }}
         className="w-full mb-4 relative overflow-hidden"
       >
-        {/* 移除固定宽高比，使用 object-contain 来适应不同尺寸的图片 */}
-        <div className="relative w-full h-full" style={{ minHeight: '600px' }}>
+        <div style={{ paddingTop: '56.25%' }} className="relative w-full">
           <img 
             src={images[currentIndex]} 
-            alt={`图片 ${currentIndex + 1}`} 
+            alt={`Image ${currentIndex + 1}`} 
             className="absolute top-0 left-0 w-full h-full object-contain"
           />
         </div>
       </div>
-      {/* 控制按钮 */}
       <div className="flex gap-4">
         <button 
           onClick={prevImage} 
@@ -49,7 +39,6 @@ const ImageSwitcher = ({ images, maxHeight = 3000 }) => {
           下一张
         </button>
       </div>
-      {/* 图片计数器 */}
       <p className="mt-2 text-sm text-gray-600">
         {currentIndex + 1} / {images.length}
       </p>
